2b0e2bacb50e235d6b24bef238135047b873ae73
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-02-14 14:40:06 +0000

    django configuration and deployment - database_url and secret_key

--- sillytalks/settings.py
+++ sillytalks/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m&xqj+mw(4@kpvv3*!s=m^&q!#y*71s-3=1mk8^syw(6dktvz9'
+# SECRET_KEY = 'django-insecure-m&xqj+mw(4@kpvv3*!s=m^&q!#y*71s-3=1mk8^syw(6dktvz9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
