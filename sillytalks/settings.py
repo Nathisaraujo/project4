8f4dd6759549b7625597a5e72d59d07e4054e7f6
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-02-14 17:21:19 +0000

    create comment class model

--- sillytalks/settings.py
+++ sillytalks/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nathisaraujo-project4-lu23mttha50.ws-eu108.gitpod.io',
                  'http://127.0.0.1:8000/',
