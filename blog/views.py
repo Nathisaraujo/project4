7dbc5080e942242feeb86ac6427c034b3ead4574
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-02-29 15:35:08 +0000

    styled like button, added like and comment count to post cards

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from .models import Post, Comment, Like
 from .forms import CommentForm
 from django.db.models import F
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
