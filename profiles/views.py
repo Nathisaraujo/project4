9cdf4e08fae9d35af7a90ec859844f255c6cdd82
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-03-05 16:21:43 +0000

    fixed userspost form

--- profiles/views.py
+++ profiles/views.py
@@ -10,21 +10,22 @@ def profile(request, username):
      # additional information such as liked posts, commented posts, etc., here
      context = {
          'user_profile': user_profile,
+         'userspost_form': UsersPostForm(),
      }
      return render(request, 'profile.html', context)
 
 def user_posts(request, username):
     if request.method == "POST":
         userspost_form = UsersPostForm(data=request.POST)
-        if users_post.is_valid():
+        if userspost_form.is_valid():
             userspost_form.save()
             messages.add_message(request, messages.SUCCESS, "Post received! We'll approve it with 2 days.")
             
     userspost_form = UsersPostForm()
 
-    return render(request, 'home/home.html',
+    return render(request, 'profile.html',
         {
-            "UsersPostForm": UsersPostForm
+            "userspost_form": userspost_form
         },
     
     )
